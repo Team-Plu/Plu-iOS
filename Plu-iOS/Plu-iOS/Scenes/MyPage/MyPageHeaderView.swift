fe4ff00d1d9d3fa47eede473fa88120ce69f4bfb
kimscastle <uiskim22@pos.idserve.net>
2023-12-05 15:01:07 +0900

    [WIP] MyPage UI구현중(#9)

--- Plu-iOS/Plu-iOS/Scenes/MyPage/MyPageHeaderView.swift
+++ Plu-iOS/Plu-iOS/Scenes/MyPage/MyPageHeaderView.swift
@@ -22,31 +22,29 @@ final class MyPageHeaderView: UIView {
         let label = UILabel()
         label.font = .suite(.head2)
         label.textColor = .designSystem(.black)
-        label.text = "Plu님"
         return label
+    }()
+    
+    private let rightArrow: UIImageView = {
+        let imageView = UIImageView(image: ImageLiterals.MyPage.arrowRightSmall900)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        // MARK: - 컴포넌트 설정
         setUI()
-        
-        // MARK: - addsubView
         setHierarchy()
-        
-        // MARK: - autolayout설정
         setLayout()
-        
-        // MARK: - button의 addtarget설정
-        setAddTarget()
-        
-        // MARK: - delegate설정
-        setDelegate()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(_ input: String) {
+        self.nickNameLabel.text = input
     }
 
 }
@@ -57,7 +55,7 @@ private extension MyPageHeaderView {
     }
     
     func setHierarchy() {
-        self.addSubviews(profileImage, nickNameLabel)
+        self.addSubviews(profileImage, nickNameLabel, rightArrow)
     }
     
     func setLayout() {
@@ -71,13 +69,11 @@ private extension MyPageHeaderView {
             make.centerY.equalToSuperview()
             make.leading.equalTo(profileImage.snp.trailing).offset(16)
         }
-    }
-    
-    func setAddTarget() {
         
-    }
-    
-    func setDelegate() {
-        
+        rightArrow.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(14)
+            make.size.equalTo(24)
+        }
     }
 }
--- Plu-iOS/Plu-iOS/Scenes/MyPage/MyPageViewController.swift
+++ Plu-iOS/Plu-iOS/Scenes/MyPage/MyPageViewController.swift
@@ -10,47 +10,151 @@ import UIKit
 
 import SnapKit
 
+struct MyPageUserData {
+    let nickName: String
+    let acceptAlarm: Bool
+    let appVersion: String
+}
+
+struct MyPageAlarmCellData {
+    let title: String
+    let acceptAlarm: Bool
+}
+
+struct MypageInfomationCellData {
+    let title: String
+}
+
+struct MypageAppVersionCellData {
+    let title: String
+    let appVersion: String?
+}
+
+enum MyPageSection {
+    case alarm(MyPageAlarmCellData)
+    case info(MypageInfomationCellData)
+    case other(MypageAppVersionCellData)
+}
+
 final class MyPageViewController: UIViewController {
+    
+    var tableData: [[MyPageSection]] = []
+    
+    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private let headerView = MyPageHeaderView(frame: .init(x: 0, y: 0, width: ScreenConstant.Screen.width, height: 100))
     
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        // MARK: - 컴포넌트 설정
         setUI()
-        
-        // MARK: - addsubView
         setHierarchy()
-        
-        // MARK: - autolayout설정
         setLayout()
-        
-        // MARK: - button의 addtarget설정
-        setAddTarget()
-        
-        // MARK: - delegate설정
         setDelegate()
-
+        setTableView()
+        setMyPageFromUserData(input: .init(nickName: "Plu님", acceptAlarm: true, appVersion: "1.0.0"))
+    }
+    
+    func setMyPageFromUserData(input: MyPageUserData) {
+        headerView.configure(input.nickName)
+        tableData = setTableViewDataFromUserData(input.acceptAlarm, input.appVersion)
+        tableView.reloadData()
+    }
+    
+    private func setTableViewDataFromUserData(_ alarmAccept: Bool, _ appVersion: String?) -> [[MyPageSection]] {
+        return [[.alarm(.init(title: "알림 설정", acceptAlarm: alarmAccept))],
+                [.info(.init(title: "FAQ")),
+                 .info(.init(title: "오픈소스 라이브러리")),
+                 .info(.init(title: "개인정보 보호 및 약관"))],
+                [.other(.init(title: "앱버전", appVersion: appVersion)),
+                 .other(.init(title: "탈퇴하기", appVersion: nil))]]
     }
 }
 
 private extension MyPageViewController {
     func setUI() {
-        
+        view.backgroundColor = .designSystem(.background)
     }
     
     func setHierarchy() {
-        
+        view.addSubview(tableView)
     }
     
     func setLayout() {
-        
-    }
-    
-    func setAddTarget() {
-        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
     
     func setDelegate() {
-        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func setTableView() {
+        tableView.sectionFooterHeight = 0
+        tableView.backgroundColor = .designSystem(.background)
+        tableView.tableHeaderView = headerView
+        MyPageAlarmTableViewCell.register(to: tableView)
+        MyPageNavigationTableViewCell.register(to: tableView)
+        MyPageOtherTableViewCell.register(to: tableView)
+        tableView.register(MyPageRoundSectionHeaderView.self, forHeaderFooterViewReuseIdentifier: MyPageRoundSectionHeaderView.identifier)
+        tableView.register(MyPageSeperateSectionHeaderView.self, forHeaderFooterViewReuseIdentifier: MyPageSeperateSectionHeaderView.identifier)
+    }
+}
+
+extension MyPageViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch tableData[indexPath.section][indexPath.row] {
+        case .alarm(let data):
+            let cell = MyPageAlarmTableViewCell.dequeueReusableCell(to: tableView)
+            cell.configure(data)
+            return cell
+        case .info(let data):
+            let cell = MyPageNavigationTableViewCell.dequeueReusableCell(to: tableView)
+            cell.configure(data)
+            return cell
+        case .other(let data):
+            let cell = MyPageOtherTableViewCell.dequeueReusableCell(to: tableView)
+            cell.configure(data)
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch tableData[indexPath.section][indexPath.row] {
+        case .alarm: return 62
+        case .info: return 52
+        case .other: return 78
+        }
+    }
+}
+
+extension MyPageViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let section = tableData[section].first else { return UIView() }
+        switch section {
+        case .alarm:
+            let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: MyPageRoundSectionHeaderView.identifier)
+            return header
+        case .info, .other:
+            let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: MyPageSeperateSectionHeaderView.identifier)
+            return header
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        guard let section = tableData[section].first else { return 0 }
+        switch section {
+        case .alarm: return 32
+        case .info, .other: return 16
+        }
     }
 }
