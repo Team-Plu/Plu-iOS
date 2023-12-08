dbfcbb294ba7295cb5d4b2db1b3c943df5c5da7a
MINJAE KIM <ffalswo2@naver.com>
2023-12-08 14:28:49 +0900

    [FEAT] 일기기록 UI 구현 (#15)

--- Plu-iOS/Plu-iOS/Scenes/Record/RecordViewController.swift
+++ Plu-iOS/Plu-iOS/Scenes/Record/RecordViewController.swift
@@ -2,7 +2,7 @@
 //  RecordViewController.swift
 //  Plu-iOS
 //
-//  Created by uiskim on 2023/12/04.
+//  Created by 김민재 on 2023/12/08.
 //  Copyright (c) 2023 Record. All rights reserved.
 //
 
@@ -12,45 +12,83 @@ import SnapKit
 
 final class RecordViewController: UIViewController {
     
+    private let totalCountLabel = PLULabel(type: .subHead1, color: .gray600, backgroundColor: .background)
+    
+    private let dateFilterButton = PLUButton(config: .bordered())
+        .setImage(image: ImageLiterals.Respone.arrowDownSmall600, placement: .trailing)
+        .setBackForegroundColor(backgroundColor: .background, foregroundColor: .gray500)
+        .setLayer(cornerRadius: 15, borderColor: .gray500)
+    
+    private let questionTableView = OthersAnswerTableView(tableViewType: .recordQuestions)
+    
+    private var datasource: UITableViewDiffableDataSource<RecordSection, RecordItem>!
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        // MARK: - 컴포넌트 설정
         setUI()
-        
-        // MARK: - addsubView
         setHierarchy()
-        
-        // MARK: - autolayout설정
         setLayout()
-        
-        // MARK: - button의 addtarget설정
-        setAddTarget()
-        
-        // MARK: - delegate설정
-        setDelegate()
-
+        setDataSource()
+        applySnapshot(.dummy())
+        configureUI(record: .dummy())
+    }
+    
+    func configureUI(record: Record) {
+        self.dateFilterButton.setText(text: record.currentDate, font: .body3)
+        self.totalCountLabel.text = "총 \(record.questions.count)개"
     }
 }
 
 private extension RecordViewController {
     func setUI() {
-        
+        view.backgroundColor = .designSystem(.background)
     }
     
     func setHierarchy() {
-        
+        self.view.addSubviews(totalCountLabel, dateFilterButton, questionTableView)
     }
     
     func setLayout() {
+        totalCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(29)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        dateFilterButton.snp.makeConstraints { make in
+            make.centerY.equalTo(totalCountLabel)
+            make.trailing.equalToSuperview().inset(20)
+        }
+        
+        questionTableView.snp.makeConstraints { make in
+            make.top.equalTo(totalCountLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
         
     }
     
-    func setAddTarget() {
-        
+    func setDataSource() {
+        self.datasource = .init(tableView: questionTableView, cellProvider: { tableView, indexPath, itemIdentifier in
+            
+            switch itemIdentifier {
+            case .question(let question):
+                let cell = RecordQuestionTableViewCell.dequeueReusableCell(to: self.questionTableView)
+                
+                cell.configureView(question)
+                return cell
+            }
+        })
     }
     
-    func setDelegate() {
+    func applySnapshot(_ records: Record) {
+        var snapshot = NSDiffableDataSourceSnapshot<RecordSection, RecordItem>()
+        snapshot.appendSections([.question])
         
+        let items = records.questions.map { question in
+            return RecordItem.question(record: question)
+        }
+        
+        snapshot.appendItems(items, toSection: .question)
+        self.datasource.apply(snapshot)
     }
 }
---
+++ Plu-iOS/Plu-iOS/Scenes/Record/TableViewCell/RecordQuestionTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  RecordQuestionTableViewCell.swift
+//  Plu-iOS
+//
+//  Created by 김민재 on 12/8/23.
+//
+
+import UIKit
+
+class RecordQuestionTableViewCell: UITableViewCell, TableViewCellRegisterDequeueProtocol {
+
+    private let questionView = QuestionView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setCell()
+        setHierarchy()
+        setLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 8, left: 21, bottom: 8, right: 21))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setCell() {
+        self.backgroundColor = .designSystem(.background)
+        self.selectionStyle = .none
+    }
+    
+    private func setHierarchy() {
+        self.contentView.addSubview(questionView)
+    }
+    
+    private func setLayout() {
+        questionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func configureView(_ question: Question) {
+        self.questionView.configureView(question)
+    }
+
+}
