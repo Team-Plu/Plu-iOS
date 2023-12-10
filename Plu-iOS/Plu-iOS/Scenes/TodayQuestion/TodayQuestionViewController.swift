193189e4bd621a54c0203c98dfc06fd1a13e8fbf
cchanmi <113524@naver.com>
2023-12-11 00:16:30 +0900

    [FEAT] 튤팁 문구 이미지 추가 (#17)

--- Plu-iOS/Plu-iOS/Scenes/TodayQuestion/TodayQuestionViewController.swift
+++ Plu-iOS/Plu-iOS/Scenes/TodayQuestion/TodayQuestionViewController.swift
@@ -12,39 +12,30 @@ import SnapKit
 
 final class TodayQuestionViewController: UIViewController {
     
-    private let questionCharcterImage = UIImageView()
+    private lazy var questionCharcterImage = UIImageView(image: self.setRandomImage())
     private let questionLabel = PLULabel(type: .head1,
                                          color: .gray700,
                                          alignment: .center,
                                          lines: 0, text: "진정한 행복이란\n 무엇이라 생각하나요?")
     private let explanationView = PLUExplanationView(text: "행복의 기준은 사람마다 달라요. \n부자가 되는 것, 여행을 다니는 것, 여유롭게 사는 것...\n여러분만의 행복의 기준은 무엇인가요? ")
+    private let seeYouTommorowImage = UIImageView(image: ImageLiterals.Main.seeYouTommorowSpeechBubble)
     private let myAnswerButton = PLUButton(config: .bordered())
         .setText(text: StringConstant.TodayQuestion.myAnswer.text, font: .title1)
-        .setBackForegroundColor(backgroundColor: .gray600, foregroundColor: .white)
-        .setLayer(cornerRadius: 8, borderColor: .gray600)
+        .setBackForegroundColor(backgroundColor: .gray600, foregroundColor: .gray50)
     private let everyAnswerButtom = PLUButton(config: .bordered())
         .setText(text: StringConstant.TodayQuestion.everyAnswer.text, font: .title1)
-        .setBackForegroundColor(backgroundColor: .gray600, foregroundColor: .white)
-        .setLayer(cornerRadius: 8, borderColor: .gray600)
+        .setBackForegroundColor(backgroundColor: .gray600, foregroundColor: .gray50)
     private let explanationLabel = PLULabel(type: .caption, color: .gray300, text: StringConstant.TodayQuestion.explanation.text)
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        // MARK: - 컴포넌트 설정
-        setUI()
-        
-        // MARK: - addsubView
-        setHierarchy()
-        
-        // MARK: - autolayout설정
-        setLayout()
-        
-        // MARK: - button의 addtarget설정
-        setAddTarget()
-        
-        // MARK: - delegate설정
-        setDelegate()
 
+        setUI()
+        setHierarchy()
+        setLayout()
+        setAddTarget()
+        setDelegate()
+        setButtonHandler()
     }
 }
 
@@ -54,14 +45,13 @@ private extension TodayQuestionViewController {
     }
     
     func setHierarchy() {
-        view.addSubviews(questionCharcterImage, questionLabel, explanationView, myAnswerButton, everyAnswerButtom, explanationLabel)
+        view.addSubviews(questionCharcterImage, questionLabel, explanationView, seeYouTommorowImage, myAnswerButton, everyAnswerButtom, explanationLabel)
     }
     
     func setLayout() {
         questionCharcterImage.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(42)
-            make.centerX.equalToSuperview()
-            make.width.height.equalTo(91)
+            make.leading.equalToSuperview().inset(103)
         }
         
         questionLabel.snp.makeConstraints { make in
@@ -71,6 +61,12 @@ private extension TodayQuestionViewController {
         
         explanationView.snp.makeConstraints { make in
             make.top.equalTo(questionLabel.snp.bottom).offset(32)
+            make.centerX.equalToSuperview()
+        }
+        
+        // output 값에 따라 isHidden 처리
+        seeYouTommorowImage.snp.makeConstraints { make in
+            make.bottom.equalTo(myAnswerButton.snp.top)
             make.centerX.equalToSuperview()
         }
         
@@ -98,5 +94,20 @@ private extension TodayQuestionViewController {
     
     func setDelegate() {
         
+    }
+    
+    func setButtonHandler() {
+        myAnswerButton.setUpdateHandler(updateHandler: { button in
+            var config = button.configuration
+            config?.baseBackgroundColor = button.isSelected
+            ? .designSystem(.gray600)
+            : .designSystem(.gray50)
+        })
+    }
+    
+    // 추후에 ViewModel로 뺄 부분
+    func setRandomImage() -> UIImage {
+        let randomIndex = Int(arc4random_uniform(UInt32(ImageDummy.imageList.count)))
+        return ImageDummy.imageList[randomIndex]
     }
 }
