39709daa13d10da7fdacd5ec9398a18578317642
kimscastle <uiskim22@pos.idserve.net>
2023-12-10 14:43:05 +0900

    [ADD] onboarding관련 string constant추가(#17)

--- Plu-iOS/Plu-iOS/Global/Constant/StringConstant.swift
+++ Plu-iOS/Plu-iOS/Global/Constant/StringConstant.swift
@@ -12,7 +12,7 @@ enum StringConstant {
     enum MyPage {
         case alarm, faq, openSource, privacy, appVersion, logOut, resign
         
-        var title: String {
+        var description: String {
             switch self {
             case .alarm: return "알림 설정"
             case .appVersion: return "앱버전"
@@ -26,12 +26,17 @@ enum StringConstant {
     }
     
     enum Onboarding {
-        case title, subTitle, placeHolder
-        var title: String {
+        case title, subTitle, placeHolder, textFieldEmpty, textFieldOver, textFieldError, nickNameNonValid, nickNameValid, none
+        var description: String? {
             switch self {
             case .title: return "닉네임을 입력해주세요"
             case .subTitle: return "닉네임을 입력하면 가입이 완료됩니다."
             case .placeHolder: return "닉네임"
+            case .textFieldEmpty: return "입력된 내용이 없습니다."
+            case .textFieldOver: return "8자 이내로 입력해주세요."
+            case .textFieldError: return "다시 시도해주세요."
+            case .nickNameNonValid: return "이미 사용 중인 닉네임입니다."
+            case .nickNameValid, .none: return nil
             }
         }
     }
