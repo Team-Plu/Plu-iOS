bd3dff2525f1a56506691577fad23c22866400a5
kimscastle <uiskim22@pos.idserve.net>
2023-12-24 12:01:16 +0900

    [FIX] 마이페이지 네비게이션 인터페이스 주석 삭제(#76)

--- Plu-iOS/Plu-iOS/Scenes/MyPage/Adaptor/MyPageNavigation.swift
+++ Plu-iOS/Plu-iOS/Scenes/MyPage/Adaptor/MyPageNavigation.swift
@@ -10,13 +10,5 @@ import Foundation
 protocol MyPageNavigation {
     var delegate: MyPageAdaptorDelegate? { get set }
     func navigation(from type: MypageNavigationType)
-//    func backButtonTapped()
-//    func myPageHeaderTapped()
-//    func resignCellTapped()
-//    func logoutCellTapped()
-//    func alarmSwitchOff()
-//    func faqCellTapped()
-//    func openSourceCellTapped()
-//    func privacyCellTapped()
 }
 
